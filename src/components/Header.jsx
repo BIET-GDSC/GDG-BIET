fd4be6600635e4d3a824a35170714291472b257f
Anshu kumar <anshu4125kumar@gmail.com>
2024-10-10 13:45:58 +0530

    added tailwind depencency and added nevbar.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import Nevbar from "./Nevbar";
+
+function Header(){
+    return <div className="h--28 w-screen flex justify-center pt-3 font-Lato">
+        <Nevbar></Nevbar>
+        </div>
+}
+
+export default Header;
